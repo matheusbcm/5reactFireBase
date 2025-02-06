9833c86a2bb039e97ec2f6cee044a4ae9e383261
matheusbcm <matheus.2013.carvalho@hotmail.com>
2025-02-06 01:28:14 -0300

    inseri as tipagens

--- src/App.tsx
+++ src/App.tsx
@@ -15,66 +15,99 @@ const firebaseApp = initializeApp({
   projectId: "projeto-filmes-tmdb",
 });
 
+interface User {
+  id: string;
+  title: string;
+  overview: string;
+  description: string;
+  year: string;
+  genre: string;
+  imgUrl: string;
+}
+
 export const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [users, setUsers] = useState([]);
+  const [id, setId] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [overview, setOverview] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [year, setYear] = useState<string>("");
+  const [genre, setGenre] = useState<string>("");
+  const [imgUrl, setImgUrl] = useState<string>("");
+  const [users, setUsers] = useState<User[]>([]);
 
   const db = getFirestore(firebaseApp);
-  const usersCollectionRef = collection(db, "users");
+  const usersCollectionRef = collection(db, "user");
 
-  async function criarDado() {
+  const criarDado = async () => {
     try {
-      const user = await addDoc(collection(db, "users"), {
-        name,
-        email,
+      const user = await addDoc(usersCollectionRef, {
+        id,
+        title,
+        overview,
+        description,
+        year,
+        genre,
+        imgUrl,
       });
 
-      console.log("dados salvos com sucessos", user);
+      console.log("Dados salvos com sucesso", user);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-  }
+  };
 
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id } as User)));
     };
     getUsers();
   }, []);
 
-  async function deleteUser(id) {
+  const deleteUser = async (id: string) => {
     const userDoc = doc(db, "users", id);
     await deleteDoc(userDoc);
-  }
+
+    setUsers(users.filter((user) => user.id !== id));
+  };
 
   return (
     <div>
       <input
-        type="text"
-        placeholder="Nome"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        type="id"
+        placeholder="id"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
       />
       <input
-        type="email"
-        placeholder="E-mail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        type="text"
+        placeholder="Título do filme"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
-      <button onClick={criarDado}>Criar dado</button>
+      <input
+        type="text"
+        placeholder="URL da imagem"
+        value={imgUrl}
+        onChange={(e) => setImgUrl(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Resumo do filme"
+        value={overview}
+        onChange={(e) => setOverview(e.target.value)}
+      />
+      <button onClick={criarDado}>Inserir Filme</button>
 
       <ul>
-        {users.map((user) => {
-          return (
-            <>
-              <li>{user.name}</li>
-              <li>{user.email}</li>
-              <button onClick={() => deleteUser(user.id)}>Deletar</button>
-            </>
-          );
-        })}
+        {users.map((user) => (
+          <div key={user.id}>
+            <img src={user.imgUrl} alt={user.title} />
+            <p>Title: {user.title}</p>
+            <p>Overview: {user.overview}</p>
+            <button onClick={() => deleteUser(user.id)}>Deletar</button>
+          </div>
+        ))}
       </ul>
     </div>
   );
